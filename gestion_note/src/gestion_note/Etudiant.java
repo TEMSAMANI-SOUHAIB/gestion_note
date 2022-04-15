af89a4b9e242b2cafec7443450a9472e95c2d146
Lenovo <Lenovo@192.168.56.1>
2022-04-15 16:53:37 +0000

    main qui reste

--- gestion_note/src/gestion_note/Etudiant.java
+++ gestion_note/src/gestion_note/Etudiant.java
@@ -1,5 +1,39 @@
 package gestion_note;
 
-public class Etudiant {
 
+public class Etudiant implements Comparable<Etudiant> {
+	private int id;
+	private String nom;
+	private double note;
+	Etudiant(int id ,String nom)
+	{
+		this.note=10;
+		this.id=id;
+		this.nom=nom;
+	};
+	
+		
+	public String toString() {
+		return "("+this.nom+ ":"+ this.note+ ")";
+		};
+		
+    public boolean equals(Object obj) {
+    	if (! (obj instanceof Etudiant) ){
+			return false;
+		}
+	return	((Etudiant)obj).id==this.id;
+    	
+    	
+    }
+
+
+	@Override
+	public int compareTo(Etudiant o1) {
+		if (o1.note==this.note) return 0;
+		if (o1.note<this.note)return 1;
+		return -1;
+	}  
+   
+    	
+    
 }
--- gestion_note/src/gestion_note/main.java
+++ gestion_note/src/gestion_note/main.java
@@ -1,5 +1,10 @@
 package gestion_note;
 
 public class main {
+	public static void{
+		Etudiant e1,e2,e3;
+		e1=new Etudiant(1);
+		
+	}
 
 }
