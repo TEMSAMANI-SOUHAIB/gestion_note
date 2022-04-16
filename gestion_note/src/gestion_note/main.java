cc516f6b31995d83715c2b1cf3a67af4afe3f55d
Lenovo <Lenovo@192.168.56.1>
2022-04-16 13:07:40 +0000

    50%

--- gestion_note/src/gestion_note/main.java
+++ gestion_note/src/gestion_note/main.java
@@ -1,10 +1,22 @@
 package gestion_note;
 
 public class main {
-	public static void{
+	public static void testetudiant(){
 		Etudiant e1,e2,e3;
-		e1=new Etudiant(1);
+		e1=new Etudiant(1,"hamid");
+		e2=new Etudiant(2, "driss");
+		e3=new Etudiant(1,"youssef");
+		e1.setnote(15);
+		e2.setnote(12);
+		e3.setnote(16);
+		Listetudiants mesetudiants=new Listetudiants();
+		mesetudiants.add(e1);
+		mesetudiants.add(e2);
+		mesetudiants.add(e3);
+		
+	}
+	
 		
 	}
 
-}
+
